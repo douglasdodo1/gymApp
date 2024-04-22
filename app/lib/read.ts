8d31c4f035866b8379163af320dcf963ce6ed3e4
douglasdodo1 <carvalhodouglas95@gmail.com>
2024-04-22 16:53:29 -0300

    feat:backend in ts

--- app/lib/read.ts
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
-
-export const getType = async () => {
-    const type =  await prisma.users.findMany();
-    return type;
-}
-
--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -2,6 +2,22 @@ import { Button, ButtonText, Text, View } from '@gluestack-ui/themed';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const HomePage = () =>{
+    const getType = async () => {
+        try {
+            // Envia uma requisição GET para o endpoint do backend
+            
+            const response = await fetch('http://150.161.11.14:3000/getTypeTrainning');
+        
+            // Converte a resposta para JSON
+            const type = await response;
+            
+            return type;
+        } catch (error) {
+            console.error('Erro ao obter tipos:', error);
+            throw error;
+        }
+    }
+
     return(
         <View style={{backgroundColor:'#1a1a1a', width:'100%', height:'100%', borderTopWidth:1, borderTopColor:'black', flex:1, alignItems:'center', }}>
             <View style={{width:'70%', height:'80%', borderWidth:1, borderColor:'red'}}>
@@ -19,10 +35,11 @@ export const HomePage = () =>{
 
                 <View style={{width:'100%', height:'10%', borderWidth:1, borderColor:'pink', flexDirection:'row', flex:1, justifyContent:'center', alignItems:'center'}}>
                     <View style={{paddingHorizontal:12}}>
-                        <Button>
+                        <Button onPress={getType}>
                             <Icon name="plus-circle" size={40} color="black" />
                         </Button>
                     </View>
+                    
                     <View>
                         <Button>
                             <Icon name="edit" size={40} color="black" />
